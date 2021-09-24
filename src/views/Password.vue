96ac59fe12efce98bb68c12dcbd881680991a14b
Abhishek-Dobliyal <abhishek.dobliyal1512@gmail.com>
2021-09-24 18:44:35 +0530

    Updated password view

--- src/views/Password.vue
+++ src/views/Password.vue
@@ -1,19 +1,19 @@
 <template>
   <Header heading="Password Generator" subHeading="Generates password as per requirements" color="slategrey" borderColor="secondary"></Header>
-  <div class="container mt-3 text-center">
-    <div class="container w-25 input-group mt-4 p-4">
-        <span class="input-group-text">Password Size</span>
-        <input v-model="passLen" type="text" class="form-control" aria-describedby="inputGroup-sizing-sm" id="passwordLen">
+  <div class="container mt-2 text-center">
+    <div class="container input-group mt-4 pt-3 w-50">
+        <span class="input-group-text">Length</span>
+        <input v-model="passLen" type="text" placeholder="Password Length" class="form-control" aria-describedby="inputGroup-sizing-sm" id="passwordLen">
     </div>
     <div class="container d-flex justify-content-center mt-4 bg-dark text-light">
         <div class="container p-3">
             <div class="form-check form-switch">
                 <input v-model="useLowercase" class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" disabled>
-                <label class="form-check-label" for="flexSwitchCheckDefault">Include Lowercase Letters</label>
+                <label class="form-check-label" for="flexSwitchCheckDefault">Include Lowercase</label>
             </div>
             <div class="form-check form-switch">
                 <input v-model="useUppercase" class="form-check-input" type="checkbox" id="flexSwitchCheckChecked">
-                <label class="form-check-label" for="flexSwitchCheckChecked">Include Uppercase Letters</label>
+                <label class="form-check-label" for="flexSwitchCheckChecked">Include Uppercase</label>
             </div>
         </div>
         <div class="container p-3">
@@ -23,13 +23,13 @@
             </div>
             <div class="form-check form-switch">
                 <input v-model="useSpecial" class="form-check-input" type="checkbox" id="flexSwitchCheckCheckedDisabled">
-                <label class="form-check-label" for="flexSwitchCheckCheckedDisabled">Include Special Characters</label>
+                <label class="form-check-label" for="flexSwitchCheckCheckedDisabled">Include Special</label>
             </div>
         </div>
     </div>
-    <div class="container w-50 input-group mt-5 pt-4">
+    <div class="container w-100 input-group mt-5 pt-4">
         <span class="input-group-text">Password</span>
-        <input v-model="password" type="text" class="form-control" disabled>
+        <input v-model="password" type="text" class="form-control w-50" disabled>
     </div>
     <button class="btn btn-outline-secondary mt-5" @click="generate">Generate Password</button>
   </div>
@@ -51,10 +51,6 @@ export default {
             useSpecial: false,
             password: ''
         }
-    },
-    updated() {
-        const inputField = document.getElementById('passwordLen');
-        inputField.defaultValue = this.passLen;
     },
     methods: {
         generatePassword(len) {
